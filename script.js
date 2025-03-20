83464ce2932e54f9b0449188e8805b11d8e732f9
AGUSTIN MAEL NO <48522428@ort>
2025-03-20 12:08:48 -0300

    casi

--- script.js
+++ script.js
@@ -1,38 +1,51 @@
-function validarNombre()
-{
-    var nombre = document.getElementById("Nombre");
-    let Mensaje = document.getElementById("ErrorNombre")
-    if(nombre.length < 3)
-    {
-        Mensaje.innerHTML="Nombre invalido";
-    }
-}
-function validarEMail()
-{
-    var email = document.getElementById("Email")
-    let Mensaje = document.getElementById("MensajeEmail")
-    if(!email.includes("@"))
-    {
-        Mensaje.innerHTML="Email invalido";
-    }
-}
-function validarContraseña()
-{
-    var contraseña = document.getElementById('Contra')
-    let Mensaje = document.getElementById("MensajeContraseña")
-    if (contraseña.length <= 8)
-    {
-        Mensaje.innerHTML="Contraseña invalida";
-    }
-}
-function validarContraseñaConfirmada()
-{
-    var contraseña = document.getElementById('Contra')
-    var confirmContra = document.getElementById('confirmarContra').value;
-    let Mensaje = document.getElementById("MensajeContraseñaConfirmada")
+const nombre = document.getElementById("Nombre");
+const email = document.getElementById("Email")
+const contraseña = document.getElementById('Contra')
+const confirmContra = document.getElementById('confirmarContra')
 
+nombre.addEventListener("input",validarNombre())
+email.addEventListener("input", validarEMail())
+contraseña.addEventListener("input", validarContraseña())
+confirmContra.addEventListener("input",validarContraseñaConfirmada())
 
-    if (Contra !== confirmContra) {
-        Mensaje.innerHTML="Contraseña incorrecta";
+    function validarNombre()
+    {   
+        let Mensaje = document.getElementById("ErrorNombre")
+        if(nombre.length < 3)
+        {
+            Mensaje.textContent = "El nombre debe tener al menos 3 caracteres" 
+        }
     }
-}
+    function validarEMail()
+    {   
+        let Mensaje = document.getElementById("MensajeEmail")
+        if(!email.includes("@"))
+        {
+            Mensaje.innerHTML="Email invalido";
+        }
+    }
+    function validarContraseña()
+    {
+        let Mensaje = document.getElementById("MensajeContraseña")
+        if (contraseña.length <= 8)
+        {
+            Mensaje.innerHTML="Contraseña invalida";
+        }
+    }
+    function validarContraseñaConfirmada()
+    {
+        //var contraseña = document.getElementById('Contra')
+        let Mensaje = document.getElementById("MensajeContraseñaConfirmada")    
+        if (Contra != confirmContra) {
+            Mensaje.innerHTML="Contraseña incorrecta";
+        }
+    }
+
+    document.getElementById('enviar').addEventListener('onclick', function(evento){
+        evento.preventDefault();
+        validarNombre()
+        validarEMail()
+        validarContraseña()
+        validarContraseñaConfirmada()
+    }
+    )
